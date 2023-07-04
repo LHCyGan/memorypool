aff0e57b0599f183a5cbd379ba40287fb382ef57
LHCyGan <autoencoder2000@163.com>
2023-07-04 13:00:49 +0800

    benchmark test

---
+++ cmake-build-debug/CMakeFiles/memorypool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memorypool.dir/benchmark.cpp.o"
+  "CMakeFiles/memorypool.dir/unitest.cpp.o"
+  "memorypool"
+  "memorypool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memorypool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
